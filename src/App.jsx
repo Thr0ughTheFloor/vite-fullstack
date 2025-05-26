b89bcdc4a387ce3bfff9468f2d7ad5d82cc43ddc
Fabio Luis Batista Santos <contatofabio2005@gmail.com>
2025-05-26 16:32:34 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
+import './App.css'
+import { api } from './api/api'
+import { useNavigate } from 'react-router'
+
+function App() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState('')
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user')
+    if(storedUser){
+      setUser(JSON.parse(storedUser))
+      navigate('/usersList')
+    }
+  }, [navigate])
+
+
+  const handleLogin = async(e) => {
+    e.preventDefault()
+    try{
+      const response = await api.post('/login', { email, password });
+      const user = response.data;
+  
+
+      localStorage.setItem('user', JSON.stringify(user))
+      setUser(user)
+      navigate('/usersList')
+    }catch(err){
+      setMessage('erro no login', err)
+    }
+  }
+
+  return (
+    <div style={{padding: '2rem'}}>
+      <form onSubmit={handleLogin}>
+        <h2>login</h2>
+        <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
+        <input type="password" placeholder='senha' value={password} onChange={(e) => setPassword(e.target.value)} required/>
+        <button type='submit'>Entrar</button>
+        <p>{message}</p>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ src/usersList.jsx
@@ -0,0 +1,41 @@
+import Style from './usersList.module.css'
+import { useEffect, useState } from 'react'
+import { api } from './api/api'
+
+
+export const UsersList = () => {
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [erro, setErro] = useState('')
+
+    useEffect(() => {
+        async function fetchUsers(){
+            try{
+              const response = await api.get('/users')
+              setUsers(response.data)
+              console.log(response.data)  
+            }catch(error){
+                setErro('Erro ao carregar usuarios' + error)
+            }finally{
+                setLoading(false)
+            }
+        }
+        fetchUsers()
+    }, [])
+
+    if(loading) return <p>Carregando Usuarios...</p>
+    if(erro) return <p>{erro}</p>
+
+    return(
+        <div style={{padding: '2rem'}}>
+            <h1>Lista de usuarios</h1>
+            <ul>
+                {users.map((item) => (
+                    <li>
+                        <strong>{item.name}</strong> - {item.id}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
